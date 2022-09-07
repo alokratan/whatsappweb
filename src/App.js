924b97c0edde5d3df927c2b778df9618883cb6d5
Alok RG <alokratan277@gmail.com>
2022-09-08 00:17:41 +0530

    whatsapp fist commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,124 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { data } from './dbs';
+import imgs from './keep.png';
+import search from './search.svg';
+import profile from './sidd.jpg';
+import {MdLock,MdMessage,MdOutlineFilterList} from 'react-icons/md';
+import {TbCircleDashed,TbDotsVertical} from 'react-icons/tb';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+ <div className="background">
+<div className="left">
+
+<div className="left-top">
+<img src={profile} alt="dp" />
+<div className="menu">
+<TbCircleDashed className='status'/>
+<MdMessage className='status'/>
+<TbDotsVertical className='status'/>
+</div>
+</div>
+<div className="searchbox">
+
+  <div className="searchandinput">
+  <img src={search} alt="" />
+  <input type="text" placeholder="Search or start new chat" />
+  </div>
+
+
+<div className="filter">
+
+<MdOutlineFilterList className='filtericon'/>
+</div>
+</div>
+
+
+<div className="container">
+
+
+
+{
+  data.map((item)=>(
+    <>
+    
+<div className="chat">
+
+<div className="dpandbox">
+<div className="dp">
+  <img src={item.img} alt="alok" />
+</div>
+
+<div className="namebox">
+  <div className="name">
+  <h4>{item.name}
+    </h4>
+    </div> 
+  <div className="msg">
+    <h5>
+    {item.msg}
+    </h5>
+    
+    </div> 
+
+</div>
+
+</div>
+
+
+<div className="timing">
+<div className="date"><h6>{item.time}</h6></div>
+<div className="msgcount"><p>{item.msgunread}</p></div>
+</div>
+
+</div>
+
+    </>
+  ))
+}
+</div>
+
+
+
+</div>
+
+<div className="right">
+
+<div className="right-top">
+  
+  </div>
+<div className="rightmid">
+<img src={imgs} alt="img"  width="400px" height="300px"/>
+
+<div className="keep">
+                <h2>WhatsApp Web
+                  </h2>
+            </div>
+            <div className="keep2">
+                <p>
+                Send and receive messages without keeping your phone online.
+                <br/>
+                Use WhatsApp on up to 4 linked devices and 1 phone at the same time.
+                   </p>  
+            </div>
+            <div className="keep3">
+                <p>
+                <span>
+                <MdLock/> 
+                  </span>End-to-end encrypted</p>  
+            </div>
+
+</div>
+<div className="bottomcolor">
+  
+</div>
+</div>
+
+ </div>
+   </>
   );
 }
 
---
+++ src/dbs.js
@@ -0,0 +1,178 @@
+export const data=[
+    {
+        name: "alok ratan Gautam",
+        status:{
+          mystatus: "My status",
+          mydate:"1 minutes ago",
+          myimg:('./img/alok.png')
+        },
+        msg: "welcome to whatsapp clone",
+        date:"1 minutes ago",
+        time:"20:22",
+        msgunread:4,
+        img:('./img/alok.png'),
+        posttext:"salted choclate bark. #delicious",
+        postimg:('./img/Posts/postsimg.png'),
+      
+      },
+      {
+        name: "raju",
+        msg: "available later",
+        date:"6 minutes ago",
+        time:"18:02",
+        msgunread:6,
+        posttext:"Trip",
+        img:('./img/m2.png'),
+        postimg:('./img/Posts/postsimg12.png')
+  
+      },
+      {
+        name: "anjali",
+        msg: "keep smile",
+        date:"Today, 08:40",
+        time:"16:46",
+        msgunread:1,
+        posttext:"Photography #nature #beach",
+        img:('./img/g1.png'),
+        postimg:('./img/Posts/postsimg2.png')
+      },
+      {
+        name: "akash Deep",
+        msg: "good",
+        date:"Today, 08:20",
+        time:"14:22",
+        msgunread:3,
+        
+        img:('./img/m3.png'),
+        postimg:('./img/Posts/postsimg3.png')
+      },
+      {
+        name: "saurabh",
+        msg: "never",
+        date:"Today, 02:12",
+        time:"13:54",
+        msgunread:7,
+        posttext:"old is gold",
+        img:('./img/m4.png'),
+        postimg:('./img/Posts/postsimg5.png')
+      },
+      {
+        name: "Jitendra",
+        msg: "are u okay?",
+        date:"Yesterday, 22:35",
+        time:"13:53",
+        posttext:"",
+        msgunread:1,
+        
+        img:('./img/takashi.jpg'),
+        postimg:('./img/Posts/postsimg6.png')
+      },
+      {
+        name: "munni Tyagi",
+        msg: "have a nice day",
+        date:"Yesterday, 22:32",
+        msgunread:4,
+        time:"13:04",
+        posttext:"shy",
+        img:('./img/g3.png'),
+        postimg:('./img/g3.png')
+      },
+      {
+        name: "Mohit",
+        msg: "please call me",
+        date:"Yesterday, 20:03",
+        msgunread:6,
+        time:"13:02",
+        img:('./img/m10.png'),
+        postimg:('./img/Posts/postsimg8.png')
+      },
+      {
+        name: "Shikha",
+        msg: "Gud night",
+        date:"Yesterday, 17:49",
+        time:"12:34",
+        msgunread:9,
+        posttext:"butterfly , flowers #nature",
+        img:('./img/g10.png'),
+        postimg:('./img/Posts/postsimg9.png')
+      },
+    
+      {
+        name: "Raj Kumar Gautam",
+        msg: "see u later",
+        date:"Yesterday, 17:49",
+        time:"Yesterday",
+        msgunread:8,
+        posttext:"blur effect",
+        img:('./img/m5.png'),
+        postimg:('./img/Posts/postsimg10.png')
+      },
+      {
+        name: "friends forever",
+        msg: "sweet",
+        date:"Yesterday, 14:12",
+        time:"Yesterday",
+        msgunread:3,
+        posttext:"yummy",
+        img:('./img/content.jpg'),
+        postimg:('./img/Posts/postsimg11.png')
+      },
+      {
+        name: "college Group",
+        msg: "good morning",
+        date:"Yesterday, 11:21",
+        time:"Yesterday",
+        posttext:"#flowers",
+        msgunread:3,
+        img:('./img/backgroun.jpg'),
+        postimg:('./img/background3.jpg')
+      },
+      {
+        name: "Manish",
+        msg: "let me know",
+        date:"Yesterday, 11:20",
+        time:"27/08/22",
+        posttext:"neon",
+        msgunread:4,
+        img:('./img/m7.png'),
+        postimg:('./img/Posts/postsimg13.png')
+      },
+      {
+        name: "Urvashi",
+        msg: "have a good day",
+        date:"Yesterday, 10:20",
+        time:"21/08/22",
+        msgunread:3,
+        img:('./img/g5.png'),
+        postimg:('./img/Posts/postsimg6.png')
+      },
+      {
+        name: "Akash",
+        msg: "best of luck",
+        date:"Yesterday, 10:01",
+        time:"20/07/22",
+        msgunread:1,
+        img:('./img/m1.png'),
+        postimg:('./img/Posts/postsimg3.png')
+      },
+      {
+        name: "Juhi",
+        msg: "iam busy right now",
+        date:"Yesterday, 09:41",
+        msgunread:5,
+        time:"27/05/22",
+        img:('./img/g9.png'),
+        postimg:('./img/Posts/postsimg13.png')
+      },
+      {
+        name: "Aman",
+        msg: "talk too you later",
+        date:"Yesterday, 08:11",
+        time:"17/06/22",
+        msgunread:13,
+        posttext:"something special",
+        img:('./img/m6.png'),
+        postimg:('./img/Posts/postsimg7.png')
+      },
+
+]
